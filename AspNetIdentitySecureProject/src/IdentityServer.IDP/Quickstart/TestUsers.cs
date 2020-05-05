861e7e65ed141938cc2cff9c38a2ff3b640d8b6e
suren-vanyan <vanyansuren@gmail.com>
2020-05-06 03:41:14 +0400

    oidc add

---
+++ AspNetIdentitySecureProject/src/IdentityServer.IDP/Quickstart/TestUsers.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServer.IDP
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users = new List<TestUser>
+        {
+            new TestUser{
+                SubjectId = Guid.NewGuid().ToString(),
+                Username = "Alice", 
+                Password = "password", 
+                Claims = 
+                {
+                   new Claim("gien_name","Alice"),
+                   new Claim("family_name","Smith")
+                }
+            },
+            new TestUser{
+                SubjectId = Guid.NewGuid().ToString(), 
+                Username = "Bob", 
+                Password = "password", 
+                Claims = 
+                {
+                    new Claim("gien_name","Bob"),
+                   new Claim("family_name","Smith")
+                }
+            }
+        };
+    }
+}
